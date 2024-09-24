77c81e248ec19c63f0edf0131cb1026747c8404e
t569 <timothyobrike@gmail.com>
2024-09-24 23:47:57 +0100

    implemented changes in main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "include/common.h"
+#include "utils/utils.h"
+
 int main(int argc, const char* argv[]){
   if(argc == 1)
  	 printf("Welcome to the Noodle compiler!\n");
   
   else{
-	// please check this
-	fprintf(stderr,"Usage: noodle has no implementation yet ;\n");
+	for(int i = 1; i < argc; i++)
+	{
+		if(validate_extension(argv[i]))
+			printf("[noodle]	Valid File: %s\n", argv[i]);
+		
+		else
+			fprintf(stderr, "[noodle]	Invalid File: %s\n", argv[i]);
+	}
+		
 }
 return 0;
 }
