325600b9becf86fbcd3c73ea05f4fdb7fe15d942
t569 <timothyobrike@gmail.com>
2024-09-24 23:47:39 +0100

    created a utils shared library

---
+++ src/utils/utils.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include "utils.h"
+
+
+int validate_extension(const char str[])
+{
+    char pattern [] = ".nd";
+    char pattern_lg[] = ".noodle";
+
+    int i, j, k;
+    for(i = 0; str[i] != '.'; i++);
+    
+    for(j = i, k = 0; pattern[k] != '\0' && str[j] == pattern[k]; j++, k++);
+
+    if(k > 0 && pattern[k] == '\0')
+        return 1;       // okay
+
+    // check the second pattern
+    else
+    {
+        for(j = i, k = 0; pattern_lg[k] != '\0' && str[j] == pattern_lg[k]; j++, k++);
+        if(k > 0 && pattern_lg[k] == '\0')
+            return 1;       // okay  
+    }
+
+    return 0;
+}
+
---
+++ src/utils/utils.h
@@ -0,0 +1,6 @@
+#ifndef NOODLE_UTILS_H
+#define NOODLE_UTILS_H
+
+#endif
+
+int validate_extension(const char []);
