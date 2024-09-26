b0f39a389fbdb6960554f6c6592eabe71aef8f42
t569 <timothyobrike@gmail.com>
2024-09-26 23:40:10 +0100

    added exit support for windows and Linux

--- src/main.c
+++ src/main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "include/common.h"
 #include "utils/utils.h"
 
 int main(int argc, const char* argv[]){
-  if(argc == 1)
- 	 printf("Welcome to the Noodle compiler!\n");
-  
-  else{
-	for(int i = 1; i < argc; i++)
+	if(argc == 1)
+		printf("Welcome to the Noodle compiler!\n");
+	
+	else if(argc == 2)
 	{
-		if(validate_extension(argv[i]))
-			printf("[noodle]	Valid File: %s\n", argv[i]);
-		
+
+		if(validate_extension(argv[1]))
+		{
+			//runFile(argv[1]);
+			// what we have for now
+			printf("[noodle:Ok]	Valid File: %s\n", argv[1]);
+		}
 		else
-			fprintf(stderr, "[noodle]	Invalid File: %s\n", argv[i]);
-	}
+		{
+			fprintf(stderr, "[noodle:Error] Invalid File: %s \n", argv[1]);
+			exit(USAGE_ERROR);
+		}
 		
-}
-return 0;
+	}
+	
+	else{
+		fprintf(stderr, "[noodle: Error] Usage: noodle <path>\n");
+		exit(USAGE_ERROR);
+	}
+	return 0;
 }
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,6 +1,18 @@
 #ifndef NOODLE_UTILS_H
 #define NOODLE_UTILS_H
 
+#ifdef _WIN32
+    #include <windows.h>
+
+    #define USAGE_ERROR ERROR_BAD_ARGUMENTS
+
+#else
+    #include <sysexits.h>
+    #define USAGE_ERROR EX_USAGE
+
 #endif
 
 int validate_extension(const char []);
+
+#endif
+
